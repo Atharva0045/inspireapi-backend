48c41b2a09345ede41b5676deadc836ab97c0d3f
Atharva <atharva0276@gmail.com>
2025-09-04 23:15:17 +0530

    Modify Test Coverage Thresholds

--- jest.config.js
+++ jest.config.js
@@ -11,10 +11,10 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
+      branches: 50,
+      functions: 65,
+      lines: 70,
+      statements: 70
     }
   }
 };
